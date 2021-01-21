c6b9ed05d629f20081a6eaff21cab86fcb95ec4e
Morgan Hein <mhein@conquestcyber.com>
2021-01-20 17:03:55 -0800

    adding bash/git

---
+++ bash/.aliases.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
+
+
+
+###############################################################################
+# Movement
+###############################################################################
+
+alias ..='cd ..'
+
+
+###############################################################################
+# Listing
+###############################################################################
+
+#listing
+alias ll="ls -lah"
+# List all files colorized in long format
+alias l="ls -lFG"
+
+# List all files colorized in long format, including dot files
+alias la="ls -laFGh"
+
+# List only directories
+alias lsd="ls -lFG | grep --color=never '^d'"
+
+# Always use color output for `ls`
+alias ls="command ls -G"
+
+
+###############################################################################
+# Networking
+###############################################################################
+
+# IP addresses
+alias localip="ipconfig getifaddr en0"
+alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+# View HTTP traffic
+alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
+alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+
+# One of @janmoesen’s ProTip™s
+for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
+	# shellcheck disable=SC2139,SC2140
+	alias "$method"="lwp-request -m \"$method\""
+done
+
+
+
+
+###############################################################################
+# Various
+###############################################################################
+
+alias untar="tar xvf"
+
+# Reload the shell (i.e. invoke as a login shell)
+alias reload='exec $SHELL -l'
+
+###############################################################################
+# Desktop
+###############################################################################
+# TODO: make this for each distro
+# Lock the screen (when going AFK)
+#alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+
+# Sleep now
+#alias gnight="pmset sleepnow"
